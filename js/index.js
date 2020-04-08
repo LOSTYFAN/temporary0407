989dec1a0b04c6382543c1c489d083d7d425032d
first <1055172199@qq.com>
2020-04-08 10:20:22 +0800

    0408

--- js/index.js
+++ js/index.js
@@ -1,9 +1,29 @@
 $(function () {
+	
+	// 加载json数据
     $.ajax({
-        url: '../data.json',
-        async: false,
+        url: './data/data.json',
         success: function (data) {
-            ipAddress = data.ip;
+			// 判断是否成功
+			if (data.msg === '成功' && data.code === '1') {
+				$("#content-action").empty();
+				$(data.result).each(function(i,d){
+					$("#content-action").append(''+
+						'<div class="item">'+
+							'<div class="item-content">'+
+								'<a href="#">'+d.title+'</a>'+
+								'<p>'+
+									'<span>'+d.time+'</span>'+
+									'<i>'+d.status_DISPLAY+'</i>'+
+								'<p>'+
+							'</div>'+
+							'<div class="item-img">'+
+								'<img src="'+d.img+'" >'+
+							'</div>'+
+						'</div>'+
+					'');
+				});
+			}   
         }
     });
 });
